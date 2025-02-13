140fbc9521cdcb8b824dbae1c5f5232031c34145
Kamylle <sharmankamylle@gmail.com>
2025-02-12 23:05:40 -0300

    matdialong and error treatment part I

--- crud-angular-spring/crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular-spring/crud-angular/src/app/courses/courses/courses.component.ts
@@ -2,8 +2,10 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
-import { Observable } from 'rxjs';
-
+import { catchError, Observable } from 'rxjs';
+import { of } from 'rxjs';
+import { MatDialog} from '@angular/material/dialog';
+import { ErrorDialogComponent } from '../../shared/components/error-dialog/error-dialog.component';
 
 @Component({
   selector: 'app-courses',
@@ -18,8 +20,24 @@ export class CoursesComponent implements OnInit {
 
   displayedColumns = ['name', 'category'];
 
-  constructor(private coursesService: CoursesService) {
-    this.courses$ = this.coursesService.list();
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog
+
+  ) {
+    this.courses$ = this.coursesService.list()
+    .pipe(
+       catchError(error => {
+         this.onError('Error loading courses');
+          return of([]);
+       })
+    );
+  }
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
   }
 
   ngOnInit(): void {
---
+++ crud-angular-spring/crud-angular/src/app/shared/components/error-dialog/error-dialog.component.ts
@@ -0,0 +1,18 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import {MAT_DIALOG_DATA} from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-error-dialog',
+  standalone: false,
+
+  templateUrl: './error-dialog.component.html',
+  styleUrl: './error-dialog.component.scss'
+})
+
+export class ErrorDialogComponent implements OnInit{
+  constructor(@Inject(MAT_DIALOG_DATA) public data: string){}
+
+  ngOnInit(): void {
+
+  }
+}
