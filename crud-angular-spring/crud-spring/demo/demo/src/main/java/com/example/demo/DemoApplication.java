7cbb87d8813266d458d5dab93227c3b070a840fd
Kamylle <sharmankamylle@gmail.com>
2025-02-25 21:50:04 -0300

    refazendo projeto spring e adicionando dependencias

--- crud-angular-spring/crud-spring/demo/demo/src/main/java/com/example/demo/DemoApplication.java
+++ crud-angular-spring/crud-spring/demo/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -4,11 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelloworldApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloworldApplication.class, args);
-		System.out.println("Ola, esse e meu primeiro projeto em spring!");
+
+		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("Ola, mundo em spring!");
 	}
 
 }
