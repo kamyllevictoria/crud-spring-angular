11376a0c8ad5201357155675fa7bfd67d7682983
Kamylle <sharmankamylle@gmail.com>
2025-07-16 23:50:33 -0300

    feat: add formarray html

--- crud-angular-spring/crud-angular/src/app/courses/containers/course-form/course-form.component.ts
+++ crud-angular-spring/crud-angular/src/app/courses/containers/course-form/course-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, NonNullableFormBuilder, UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import { FormGroup, NonNullableFormBuilder, UntypedFormArray, Validators } from '@angular/forms';
 import { CoursesService } from '../../services/courses.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Location } from '@angular/common';
@@ -11,7 +11,7 @@ import { Lesson } from '../../model/lesson';
   selector: 'app-course-form',
   standalone: false,
   templateUrl: './course-form.component.html',
-  styleUrl: './course-form.component.scss'
+  styleUrls: ['./course-form.component.scss']
 })
 export class CourseFormComponent implements OnInit {
 
@@ -37,15 +37,14 @@ export class CourseFormComponent implements OnInit {
     });
     console.log(this.form);
     console.log(this.form.value)
-
   }
 
   private retrieveLessons(course: Course){
     const lessons = [];
-    if(course?.lessons){
+    if(course?.lessons && course.lessons.length > 0){ 
       course.lessons.forEach(lesson => lessons.push(this.createLesson(lesson)))
-    } else{
-      lessons.push(this.createLesson);
+    } else {
+      lessons.push(this.createLesson());
     }
     return lessons;
   }
@@ -58,6 +57,9 @@ export class CourseFormComponent implements OnInit {
     });
   }
 
+  getLessonsFormArray(){
+    return (<UntypedFormArray>this.form.get('lessons')).controls
+  }
 
   onSubmit() {
     this.service.save(this.form.value)
--- crud-angular-spring/crud-angular/src/app/courses/courses.module.ts
+++ crud-angular-spring/crud-angular/src/app/courses/courses.module.ts
@@ -1,23 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule, Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
-
-import { CoursesRoutingModule } from './courses-routing.module';
-import { CoursesComponent } from './containers/courses/courses.component';
-import { AppMaterialModule } from '../shared/app-material/app-material.module';
-import { SharedModule } from '../shared/shared.module';
-import { CourseFormComponent } from './containers/course-form/course-form.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatCardModule } from '@angular/material/card';
-import { CoursesListComponent } from './components/courses-list/courses-list.component';
+import { NgModule } from "@angular/core"
+import { CommonModule, Location, LocationStrategy, PathLocationStrategy } from "@angular/common"
+import { CoursesRoutingModule } from "./courses-routing.module"
+import { CoursesComponent } from "./containers/courses/courses.component"
+import { AppMaterialModule } from "../shared/app-material/app-material.module"
+import { SharedModule } from "../shared/shared.module"
+import { CourseFormComponent } from "./containers/course-form/course-form.component"
+import { ReactiveFormsModule } from "@angular/forms"
+import { MatFormFieldModule } from "@angular/material/form-field"
+import { MatInputModule } from "@angular/material/input"
+import { MatCardModule } from "@angular/material/card"
+import { CoursesListComponent } from "./components/courses-list/courses-list.component"
+import { RouterModule } from "@angular/router"
 
 @NgModule({
-  declarations: [
-    CoursesComponent,
-    CourseFormComponent,
-    CoursesListComponent,
-  ],
+  declarations: [CoursesComponent, CourseFormComponent, CoursesListComponent],
   imports: [
     CommonModule,
     CoursesRoutingModule,
@@ -26,11 +22,9 @@ import { CoursesListComponent } from './components/courses-list/courses-list.com
     ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule,
-    MatCardModule
+    MatCardModule,
+    RouterModule,
   ],
-  providers: [
-    Location,
-    { provide: LocationStrategy, useClass: PathLocationStrategy }
-  ]
+  providers: [Location, { provide: LocationStrategy, useClass: PathLocationStrategy }],
 })
-export class CoursesModule { }
+export class CoursesModule {}
