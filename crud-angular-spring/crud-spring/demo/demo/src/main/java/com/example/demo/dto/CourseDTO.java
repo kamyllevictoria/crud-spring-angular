d537b71c4988be7e172f2a1037c0afe8dbd2315a
Kamylle <sharmankamylle@gmail.com>
2025-07-22 19:27:41 -0300

    feat: validating enums with java bean validation

--- crud-angular-spring/crud-spring/demo/demo/src/main/java/com/example/demo/dto/CourseDTO.java
+++ crud-angular-spring/crud-spring/demo/demo/src/main/java/com/example/demo/dto/CourseDTO.java
@@ -1,5 +1,7 @@
 package com.example.demo.dto;
 
+import com.example.demo.enums.Category;
+import com.example.demo.enums.validation.ValueOfEnum;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.*;
@@ -10,7 +12,7 @@ public record CourseDTO(
         @JsonProperty("_id")Long id,
         @NotBlank @NotNull @Size(min = 5, max = 100)String name,
         @NotNull @Pattern(regexp = "Back-end|Front-end|Data")
-        String category, @Pattern(regexp = "Active|Inactive")
+        String category, @ValueOfEnum(enumClass = Category.class)
         String status,
         @NotNull @NotEmpty @Valid List<LessonDTO> lessons){
 
